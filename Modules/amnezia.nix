4a1f68274674c96b50bdfc5db9acf0bacbb63ffd
kenlog <example.com>
2024-08-22 22:01:40 +0300

    changed zen kernel to latest 4

--- Modules/amnezia.nix
+++ Modules/amnezia.nix
@@ -14,7 +14,7 @@
     })
   ];
 
-  boot.kernelPackages = pkgs.amnezia.linuxKernel.packages.linuxPackages_latest;
+  boot.kernelPackages = pkgs.amnezia.linuxKernel.packages.linux_lts;
   boot.extraModulePackages = [config.boot.kernelPackages.amneziawg];
   environment.systemPackages = [
     pkgs.amnezia.amneziawg-tools
