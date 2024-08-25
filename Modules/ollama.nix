23cd524298e16c780c1acba816439e41acc8717f
Kenlog <kenlog@example.com>
2024-08-25 21:27:39 +0300

    changed ollama to unstable

--- Modules/ollama.nix
+++ Modules/ollama.nix
@@ -6,7 +6,7 @@
 }: {
   services.ollama = {
     #package = pkgs.unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
-    package = pkgs.ollama;
+    package = pkgs.unstable.ollama;
     enable = true;
     acceleration = "cuda";
     # for checking ig ollama is running: http://127.0.0.1:11434/
