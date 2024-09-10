062a65f12444e666c1c1a456453f356165fec161
Kenlog <kenlog@example.com>
2024-09-10 21:09:05 +0300

    fixed wezterm disappearing cursor

--- Modules/wezterm.nix
+++ Modules/wezterm.nix
@@ -7,11 +7,12 @@
     programs.wezterm = {
       enable = true;
       extraConfig = ''
-                local wezterm = require 'wezterm'
+       local wezterm = require 'wezterm'
 
         local config = wezterm.config_builder()
 
         config.front_end = "WebGpu"
+        config.xcursor_theme = "Vimix-cursors";
 
         return config
 
